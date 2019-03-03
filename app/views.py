1bf6ecac9c1b30622b7f6e2727585b43b79c7c30
David Bryson <david.bryson@mymona.uwi.edu>
2019-03-03 00:39:42 +0000

    step 9 completed

--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm
 from app.models import UserProfile
-
+from werkzeug.security import check_password_hash
 
 ###
 # Routing for your application.
@@ -31,23 +31,32 @@ def about():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if request.method == "POST":
-        # change this to actually validate the entire form submission
-        # and not just one field
-        if form.username.data:
-            # Get the username and password values from the form.
+    if request.method == "POST" and form.validate_on_submit():
+        
+        if form.username.data and form.password.data:
 
-            # using your model, query database for a user based on the username
-            # and password submitted. Remember you need to compare the password hash.
-            # You will need to import the appropriate function to do so.
-            # Then store the result of that query to a `user` variable so it can be
-            # passed to the login_user() method below.
+            username = form.username.data
+            password = form.password.data
+            user = UserProfile.query.filter_by(username=username).first()
+            
+            if user is not None and check_password_hash(user.password, password):
+                
+                remember_me = False
 
-            # get user id, load into session
-            login_user(user)
-
+                if 'remember_me' in request.form:
+                    remember_me = True
+                    
+            login_user(user,remember=remember_me)
+                
+            flash('Logged in successfully.', 'success')
+            next_page = request.args.get('next')
+            return redirect(next_page or url_for('home'))
+            
+        else:
+            flash('Username or Password is incorrect.', 'danger')
+            
             # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+    flash_errors(form)
     return render_template("login.html", form=form)
 
 
@@ -78,6 +87,11 @@ def add_header(response):
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'public, max-age=0'
     return response
+
+
+
+
+
 
 
 @app.errorhandler(404)
